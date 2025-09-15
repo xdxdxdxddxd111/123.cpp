87692bac739edc8d665e73ec45fdc742cbe761d5
xdxdxdxddxd111 <tesyganishe@mail.ru>
2025-09-15 12:55:13 +0300

    Create 321.cpp

---
+++ 321.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+void task_1() {
+    std::cout <<"Hello, World!" << std::endl;
+}
+
+void task_2() {
+    int x = 5;
+    std::cout << "x = " << x << std::endl;
+}
+
+void task_3() {
+    int age;
+        std::cout << "Enter your age: ";
+        std::cin >> age; 
+        std::cout << "Your " << age << " age" << std::endl;
+}
+
+void task_4() {
+    int num1, num2, sum;
+
+    std::cout << "Enter two number: ";
+    std::cin >> num1 >> num2;
+
+    sum = num1 + num2;
+
+    std::cout << "Sum: " << sum << std::endl;
+}
+
+void task_5() {
+     const double PI = 3.14159265358979323846; 
+     
+    double radius; 
+
+    std::cout << "Enter radius: "; 
+    std::cin >> radius;
+
+    double circumference = 2 * PI * radius; 
+
+    std::cout << "Weight: " << circumference << std::endl;
+}
+
+
+void task_6() {
+    int a = 10;
+    int b = 3;
+
+    int quotient = a / b;
+    int remainder = a % b;
+
+    std::cout << "a / b: " << quotient << std::endl;
+    std::cout << "a % b: " << remainder << std::endl;
+}
+
+
+int main() {
+    task_2();
+    task_1();
+    task_3();
+    task_4();
+    task_5();
+    task_6();
+
+    return 0;
+}
